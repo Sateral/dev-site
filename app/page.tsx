1c456dc11f6fb9016e40d87b5b49c8da7cc126c6
Daniel <kopylenkod@gmail.com>
2025-10-17 15:19:49 -0400

    created about me

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import About from '@/app/_sections/about';
+
 export default function Home() {
   return (
-    <div className="grainy font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start"></main>
+    <div className="grainy font-sans min-h-screen">
+      <main className="flex flex-col p-6">
+        <About />
+      </main>
     </div>
   );
 }
