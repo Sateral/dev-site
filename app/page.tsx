88a7919bf46ff0e65f822add17d113f8fce132f7
Daniel <kopylenkod@gmail.com>
2025-10-18 15:19:05 -0400

    fixed grainy bg

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import About from '@/app/_sections/about';
 
 export default function Home() {
   return (
-    <div className="grainy font-sans min-h-screen">
+    <div className="font-sans min-h-screen">
       <main className="flex flex-col p-6">
         <About />
       </main>
